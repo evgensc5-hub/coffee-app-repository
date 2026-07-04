fc220d2c374c064fbdd1b5a09b00ff4ac7c4c562
Eugen Scutaru <evgensc5@gmail.com>
2026-07-04 11:45:03 +0300

    Populate DrimkCoffee

--- src/DrimkCoffee.java
+++ src/DrimkCoffee.java
@@ -1,2 +1,16 @@
 public class DrimkCoffee {
+    void drinkCoffee(Coffee coffee) throws TemperatureException, ConcentrationException{
+        if coffee.getTemp() > 65) {
+    throw new TemperatureException(coffee.getTemp(),"Cafeaua e prea fierbinte. 0 aruncam!");
+
+        }else {
+            System.out.println("t: "+ coffee.getTemp());
+        }
+        if(coffee.getConc() > 55){
+            throw new ConcentrationException(coffee.getConc(), "Concentratia mare. 0 aruncam!");
+
+        }else {
+            System.out.println("c: " + coffee.getConc());
+        }
+    }
 }
