e3e556149177c5d8d7ae3b0c57f369d043069f36
Eugen Scutaru <evgensc5@gmail.com>
2026-07-04 11:23:12 +0300

    Populate Coffee class

--- src/Coffee.java
+++ src/Coffee.java
@@ -3,4 +3,25 @@ public class Coffee {
     private int temp;
     private int conc;
 
+    //constructorul
+    public Coffee(int temp, int conc) {
+        this.temp = temp;
+        this.conc = conc;
+    }
+    //getters
+
+
+    public int getConc() {
+        return conc;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+    @Override new*
+
+    @Override
+    public String toString() {
+        return "[temperatura cafelei = " +temp + ":concentratia = "+ conc + "]";
+    }
 }
