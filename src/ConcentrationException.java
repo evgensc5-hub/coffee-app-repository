771e8c80d7da8bf7344a491d39f58d8031f07465
Eugen Scutaru <evgensc5@gmail.com>
2026-07-04 11:34:41 +0300

    populate ConcentrationException

--- src/ConcentrationException.java
+++ src/ConcentrationException.java
@@ -1,2 +1,14 @@
 public class ConcentrationException extends Exception{
+    // props
+    int c;
+
+    public TemperatureException(int c, String msg) {
+        super (msg);this.c = c;
+
+    }
+
+    public int getC() {
+        return c;
+    }
+}
 }
